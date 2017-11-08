7a1a4bb3947cf1018e39e5866d0ae101fa702657
ksari17 <31630024+ksari17@users.noreply.github.com>
2017-11-08 12:58:43 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,16 @@
+## Exploratory Data Analysis Project 1
+## Kriti Sarin
+
+## Data has been downloaded and unzipped
+
+## Read data files
+dataset <- read.table("household_power_consumption.txt", header=TRUE, na.strings="?", sep=";")
+
+## Subset and reformat data between 1/2/2007 and 2/2/2007
+data <- subset(data, subset = (Date >= "1/2/2007" & Date <= "2/2/2007"))
+data$DateTime <- strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")
+
+## Generate Plot 2
+png("plot2.png", width = 480, height = 480)
+plot(data$DateTime, data$Global_active_power, type = "l",ylab = "Global Active Power (kilowatts)", xlab = NA)
+dev.off()

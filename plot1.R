cd447792551c47fe5f218078dde840b766d7fa8b
ksari17 <31630024+ksari17@users.noreply.github.com>
2017-11-08 13:10:16 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,8 +6,9 @@
 ## Read data files
 dataset <- read.table("household_power_consumption.txt", header=TRUE, na.strings="?", sep=";")
 
-## Subset data between 1/2/2007 and 2/2/2007
+## Subset and reformat data between 1/2/2007 and 2/2/2007
 data <- subset(data, subset = (Date >= "1/2/2007" & Date <= "2/2/2007"))
+data$DateTime <- strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")
 
 ## Generate Plot 1
 png("plot1.png", width = 480, height = 480)
